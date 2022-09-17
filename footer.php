8d79c407dbddb99bb4543a335178bdffe5756ada
unknown <facuhomola22@gmail.com>
2022-09-17 19:54:15 -0300

    se añade cabecera y pie de pagina --> Colocar contenido del sitio a modo ejemplo

---
+++ footer.php
@@ -0,0 +1,15 @@
+<footer>
+    
+    <div class="copyright py-3 text-white mt-4 bg-dark border-5">
+        <div class="container-fluid text-center pt-1 pb-1">
+          <h6>Cursolandia</h6>           
+        </div>
+
+        <div class="footer-copyright text-center py-1">© <script>
+            document.write(new Date().getFullYear());
+        </script> Copyright - Todos los derechos reservados
+        <h6> Facundo Homola</h6>
+        </div>
+    </div>
+
+    </footer>
---
+++ header.php
@@ -0,0 +1,35 @@
+<nav id="barraNavegacion" class="navbar navbar-dark navbar-expand-lg">
+  <div class="container-fluid contenedores">
+    <a class="navbar-brand" href="#">Cursolandia</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item dropdown">
+          <a class="nav-link active dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Categorías
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="">Negocios</a></li>
+            <li><a class="dropdown-item" href="#">Empresas</a></li>
+            <li><a class="dropdown-item" href="#">Medicina</a></li>
+            <li><a class="dropdown-item" href="#">Programación</a></li>
+            <li><a class="dropdown-item" href="#">VideoJuegos</a></li>
+            <li><a class="dropdown-item" href="#">Diseño Gráfico</a></li>
+            <li><a class="dropdown-item" href="#">Idiomas</a></li>
+          </ul>
+        </li>
+      </ul>
+      <form id="search" class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="¿Qué desea buscar?" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+            <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/></svg></button>
+      </form>
+    </div>
+    <div class="pl-2 pr-2 ms-2">
+        <button id="btn-sesion" type="button" class="btn">Iniciar Sesión</button>
+        <button id="btn-registro" type="button" class="btn">Registrarse</button>
+    </div>
+  </div>
+</nav>
